85e52544453a9e773fbbce852a5a4e301e717acd
Ivan <BolsunIvan@yandex.by>
2021-05-26 20:20:09 +0300

    Basket client side

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {BreadcrumbDefinition, BreadcrumbService} from "xng-breadcrumb";
 import {Observable} from "rxjs";
+import {BasketService} from "./basket/basket.service";
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,20 @@ import {Observable} from "rxjs";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'SkiNet';
+  title = 'DriveMoto';
   breadcrumb$: Observable<BreadcrumbDefinition[]>;
 
-  constructor(private bcService: BreadcrumbService) { }
+  constructor(private bcService: BreadcrumbService, private basketService: BasketService) { }
 
   ngOnInit(): void {
-    this.breadcrumb$ = this.bcService.breadcrumbs$
+    this.breadcrumb$ = this.bcService.breadcrumbs$;
+    const basketId = localStorage.getItem('basket_id');
+    if(basketId){
+      this.basketService.getBasket(basketId).subscribe(() => {
+        console.log('initialised basket');
+      }, error => {
+        console.log(error);
+      })
+    }
   }
 }
--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import {BreadcrumbService} from "xng-breadcrumb";
+import {BasketService} from "../../basket/basket.service";
 
 @Component({
   selector: 'app-product-details',
@@ -11,13 +12,15 @@ import {BreadcrumbService} from "xng-breadcrumb";
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
+  quantity = 1;
 
   constructor(
     private shopService: ShopService,
     private activatedRoute: ActivatedRoute,
-    private bcService: BreadcrumbService
+    private bcService: BreadcrumbService,
+    private basketService: BasketService
   ) {
-    this.bcService.set('@productDetails', '');
+    this.bcService.set('@productDetails', ' ');
   }
 
   ngOnInit(): void {
@@ -36,5 +39,19 @@ export class ProductDetailsComponent implements OnInit {
           console.log(error);
         }
       );
+  }
+
+  addItemToBasket() {
+    this.basketService.addItemToBasket(this.product, this.quantity)
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  decrementQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
   }
 }
